49e75f2ab1ec03b469a36b5554f486c382796577
whosgotch <komovaar@gmail.com>
2024-02-06 16:56:19 +0200

    added onchange method for garden fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -39,3 +39,12 @@ class EstateProperty(models.Model):
           line.best_price = max(line.mapped("offer_ids.price")) or 0
         else:
           line.best_price = 0
+
+  @api.onchange("garden")
+  def _onchange_garden(self):
+    if self.garden:
+      self.garden_area = 10 
+      self.garden_orientation = "north"
+    else:
+      self.garden_area = 0
+      self.garden_orientation = ""
