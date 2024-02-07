0df6c3652d184ea7aa2ae249c5e96cfff711a12c
whosgotch <komovaar@gmail.com>
2024-02-07 13:22:12 +0200

    added customization for tags

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -6,6 +6,7 @@ class EstatePropertyTag(models.Model):
     _order = "name"
 
     name = fields.Char(required=True)
+    color = fields.Integer()
 
     _sql_constraints = [
         ('unique_name', 'UNIQUE(name)', 'Property tag must be unique.')
