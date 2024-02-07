dbae1013d8a18f2376163ee711964bc58a46c3e0
whosgotch <komovaar@gmail.com>
2024-02-07 13:05:46 +0200

    added order to all estate models

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -3,6 +3,7 @@ from odoo import models, fields
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Real Estate Property Type"
+    _order = "name"
 
     name = fields.Char(required=True)
     property_ids = fields.One2many('estate.property', 'property_type_id')
