33883b140c58425ede71e1df98d37cb40d27992d
whosgotch <komovaar@gmail.com>
2024-02-07 12:59:24 +0200

    added property type tree for form view

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -5,6 +5,7 @@ class EstatePropertyType(models.Model):
     _description = "Real Estate Property Type"
 
     name = fields.Char(required=True)
+    property_ids = fields.One2many('estate.property', 'property_type_id')
 
     _sql_constraints = [
         ('unique_name', 'UNIQUE(name)', 'Property type must be unique.')
